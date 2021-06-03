e4c0999261f049f44693d2ca133b4d5637374af9
Rex Hu <huzhengyu1986@gmail.com>
2021-06-02 21:36:26 -0400

    Simple updates

--- src/main/scala/WordCountStream.scala
+++ src/main/scala/WordCountStream.scala
@@ -1,19 +1,31 @@
-import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.streaming._
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 object WordCountStream {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[2]").appName("Word Count in Stream").getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
 
-    val lines: DataFrame = spark.readStream.format("socket")
+    import spark.implicits._
+
+    val streamDF: DataFrame = spark.readStream.format("socket")
       .option("host", "localhost")
       .option("port", 9999)
       .load()
 
-    val streamingQuery: StreamingQuery = lines.writeStream
+    val wordsDF: DataFrame = streamDF
+      .withColumn("wordArray", split($"value", " "))
+      .withColumn("word", explode($"wordArray"))
+
+    val countDF = wordsDF
+      .groupBy($"word")
+      .agg(count("value").alias("Count"))
+
+    val streamingQuery: StreamingQuery = countDF.writeStream
       .format("console")
-      .outputMode("append")
+      .outputMode("complete")
       .start()
 
     streamingQuery.awaitTermination()
